6107355c5bad720809ac21a5d93c565f35357112
Madeline <moon.madeline@gmail.com>
2023-02-14 10:27:42 -0600

    Updates that toggle classes and trigger content

--- drop-down/script.js
+++ drop-down/script.js
@@ -2,8 +2,28 @@
 // onclick toggle class of all siblings ul.collapse
 
 let menu = document.getElementById('menu');
-let menuList = document.getElementById('menu-list');
+let menuList = document.getElementById('main-menu');
 
 menu.addEventListener('click', () =>{
-    menuList.classList.toggle('collapse');
+    collapseMenu();
 });
+
+let collapseMenu = () => menuList.classList.toggle('collapse');
+
+let menuItems = document.querySelectorAll('.link');
+
+menuItems.forEach(listItem => listItem.addEventListener('click', () => {
+    resetActive();
+    listItem.classList.toggle('active');
+    collapseMenu();
+    triggerContent(listItem.id);
+}))
+
+const resetActive = () => {
+    let activeItems = document.querySelectorAll('.active'); 
+    activeItems.forEach(activeItem => activeItem.classList.toggle('active'));
+}
+
+const triggerContent = (id) => {
+    console.log(`will trigger content for this page: ${id}`);
+}
